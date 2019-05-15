5c39c3c6d1545d2ac42443590b6dff08546e03e8
joel <joel.gerbore@laser-group.com>
2019-05-15 14:58:26 +0200

    user navigation: aggiunta chiamata (verifica cell hermes)

--- core/src/main/kotlin/com/krake/core/widget/UserNavigationMenuView.kt
+++ core/src/main/kotlin/com/krake/core/widget/UserNavigationMenuView.kt
@@ -106,7 +106,7 @@ class UserNavigationMenuView @JvmOverloads constructor(
         dataConnection?.model?.observeForever(this)
         LoginManager.shared.isLogged.observeForever(loginObserver)
         if (LoginManager.shared.isLogged.value == true) {
-            updateUI(true, null)
+            updateUI(true, currentUser)
         }
         Signaler.shared.registerApiEndListener(getString(R.string.orchard_api_path_content_modify), this)
     }
