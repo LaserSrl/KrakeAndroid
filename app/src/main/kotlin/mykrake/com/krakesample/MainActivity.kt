57c31123e7b8d84d719a8bd4e38a7f5c2dce9f32
joel <joel.gerbore@laser-group.com>
2019-08-07 13:41:46 +0200

    Rimosso elmento errato branch

--- app/src/main/kotlin/mykrake/com/krakesample/MainActivity.kt
+++ app/src/main/kotlin/mykrake/com/krakesample/MainActivity.kt
@@ -99,9 +99,6 @@ class MainActivity : LoginAndPrivacyActivity(), OnContentItemSelectedListener, O
 
     }
 
-    override fun onContentItemNoMoreInEvidence(senderFragment: Any, contentItem: ContentItem) {
-    }
-
     override fun update(observable: Observable, data: Any) {
 
     }
