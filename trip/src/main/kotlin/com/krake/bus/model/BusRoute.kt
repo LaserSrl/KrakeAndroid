1018c52e57848d26a1f6eea44efefef211e81b19
joel <joel.gerbore@laser-group.com>
2019-08-07 15:56:13 +0200

    bus route: long name

--- trip/src/main/kotlin/com/krake/bus/model/BusRoute.kt
+++ trip/src/main/kotlin/com/krake/bus/model/BusRoute.kt
@@ -1,13 +1,11 @@
 package com.krake.bus.model
 
 import com.krake.core.model.RecordWithStringIdentifier
-import io.realm.RealmModel
-import io.realm.RealmObject
-import io.realm.RealmObjectChangeListener
-import io.realm.annotations.RealmClass
 
 interface BusRoute: RecordWithStringIdentifier {
     val color: Int
 
     val mode: RouteMode
+
+    val longName: String
 }
